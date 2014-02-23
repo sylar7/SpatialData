fecf9728d7af5354ead98f6b4e39d4f97c1c4799
sylar7 <andy34021@126.com>
2014-02-23 16:43:22 +0800

    GetMeetingPoints add some code

--- GetMeetingPoint/src/GetMeetingPoint.java
+++ GetMeetingPoint/src/GetMeetingPoint.java
@@ -3,6 +3,17 @@ class GetMeetingPoint{
 		
 	}
 	
+	public static void Solve(int x1, int y1, int x2, int y2){
+		
+	}
+	public static float getMin(float d1, float d2){
+		return (d1 <= d2) ? d1 : d2;
+	}
+	
+	public static boolean isInMBR(int x, int y, MBR mbr){
+		return (x >= mbr.lower_x && x <= mbr.upper_x && y >= mbr.lower_y && y <= mbr.upper_y);
+	}
+	
 	public static class MBR{
 	private int id;
 	private float lower_x;
